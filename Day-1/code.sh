d621c75f4dfb32b2453a284008af08daa188a6d8
saurav <pinkunpadhy1998@gmail.com>
2025-08-26 22:02:07 +0530

    Day 1 completed

---
+++ Day-1/code.sh
@@ -0,0 +1,6 @@
+#Jump to the Server
+# ssh <username>@<server-ip/server-name>
+ssh stapp03@banner
+# Create a user with non interactive shell
+# sudo useradd -s /sbin/nologin student
+sudo useradd -s /sbin/nologin john
